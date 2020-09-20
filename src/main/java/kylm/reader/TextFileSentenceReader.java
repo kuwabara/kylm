4ae6095c8139e9e1385a9dc2d54f00fbc607b753
KUWABARA Hiroaki <kuwabara@nanzan-u.ac.jp>
2020-09-20 18:18:33 +0900

    Modify to call this constructor

--- src/main/java/kylm/reader/TextFileSentenceReader.java
+++ src/main/java/kylm/reader/TextFileSentenceReader.java
@@ -37,18 +37,18 @@ import kylm.util.KylmTextUtils;
  *
  */
 public class TextFileSentenceReader implements SentenceReader {
-	
+
 	// iterator implementation
 	private class TFSLIterator implements Iterator<String[]> {
-		
+
 		private BufferedReader reader = null;
 		private String divider = null;
-		
+
 		public TFSLIterator(File file, String divider) throws FileNotFoundException {
 			reader = new BufferedReader(new FileReader(file));
 			this.divider = divider;
 		}
-		
+
 		@Override
 		public boolean hasNext() {
 			try {
@@ -64,7 +64,7 @@ public class TextFileSentenceReader implements SentenceReader {
 				String line = reader.readLine();
 				if(line.length() == 0)
 					return new String[0];
-				StringTokenizer st=new StringTokenizer(line,divider); 
+				StringTokenizer st=new StringTokenizer(line,divider);
 				String[] ret = new String[st.countTokens()];
 				for(int i = 0; i < ret.length; i++)
 					ret[i] = st.nextToken();
@@ -78,26 +78,23 @@ public class TextFileSentenceReader implements SentenceReader {
 		public void remove() {
 			throw new UnsupportedOperationException("Remove is not implemented");
 		}
-		
+
 	}
-	
+
 	private File file = null;
 	private String divider = null;
-	
+
 	/**
-	 * The constructor, saves the file and uses a single space as the default divider 
+	 * The constructor, saves the file and uses a single space as the default divider
 	 * @param fileName The name of the file to be opened
 	 * @throws IOException if the file doesn't exist or is unreadable
 	 */
 	public TextFileSentenceReader(String fileName) throws IOException {
-		file = new File(fileName);
-		if(!file.canRead())
-			throw new IOException("File "+fileName+" does not exist or is unreadable");
-		divider = KylmTextUtils.whiteSpaceString;
+	    this(fileName, KylmTextUtils.whiteSpaceString);
 	}
-	
+
 	/**
-	 * The constructor, saves the file name and uses the passed in divider 
+	 * The constructor, saves the file name and uses the passed in divider
 	 * @param fileName The name of the file to be opened
 	 * @param divider A regular expression that is used to divide strings in the corpus
 	 * @throws IOException if the file doesn't exist or is unreadable
