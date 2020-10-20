ab621e59f87a51918aee9254181141022a5d4edb
KUWABARA Hiroaki <kuwabara@nanzan-u.ac.jp>
2020-10-20 21:52:13 +0900

    Remove unnecessary code

--- src/main/java/kylm/reader/TextFileSentenceReader.java
+++ src/main/java/kylm/reader/TextFileSentenceReader.java
@@ -21,13 +21,8 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
 package kylm.reader;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.UncheckedIOException;
-import java.lang.Thread.UncaughtExceptionHandler;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -35,11 +30,7 @@ import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
-import java.util.StringTokenizer;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import kylm.util.KylmTextUtils;
 
 /**
  * Implementation of a loader that loads from a text file
@@ -47,70 +38,14 @@ import kylm.util.KylmTextUtils;
  *
  */
 public class TextFileSentenceReader implements SentenceReader {
-
-	// iterator implementation
-	private class TFSLIterator implements Iterator<String[]> {
-
-		private BufferedReader reader = null;
-		private String divider = null;
-
-		public TFSLIterator(File file, String divider) throws FileNotFoundException {
-			reader = new BufferedReader(new FileReader(file));
-			this.divider = divider;
-		}
-
-		@Override
-		public boolean hasNext() {
-			try {
-				return reader.ready();
-			} catch (IOException e) {
-				return false;
-			}
-		}
-
-		@Override
-		public String[] next() {
-			try {
-				String line = reader.readLine();
-				if(line.length() == 0)
-					return new String[0];
-				StringTokenizer st=new StringTokenizer(line,divider);
-				String[] ret = new String[st.countTokens()];
-				for(int i = 0; i < ret.length; i++)
-					ret[i] = st.nextToken();
-				return ret;
-			} catch (Exception e) {
-				return null;
-			}
-		}
-
-		@Override
-		public void remove() {
-			throw new UnsupportedOperationException("Remove is not implemented");
-		}
-
-	}
-
-	private File file = null;
-	private String divider = null;
-    private List<Path> filePaths;
-
-	/**
-	 * The constructor, saves the file and uses a single space as the default divider
-	 * @param fileName The name of the file to be opened
-	 * @throws IOException if the file doesn't exist or is unreadable
-	 */
-	public TextFileSentenceReader(String[] fileName) throws IOException {
-	    this(fileName, KylmTextUtils.whiteSpaceString);
-	}
+	private List<Path> filePaths;
 
 	/**
 	 * The constructor, saves the file name and uses the passed in divider
 	 * @param fileName The name of the file to be opened
-	 * @param divider A regular expression that is used to divide strings in the corpus
 	 * @throws IOException if the file doesn't exist or is unreadable
 	 */
-	public TextFileSentenceReader(String[] fileName, String divider) throws IOException {
+	public TextFileSentenceReader(String[] fileName) throws IOException {
 	    filePaths = Arrays.stream(fileName)
 	        .map(Paths::get)
 	        .collect(Collectors.toList())
@@ -123,8 +58,6 @@ public class TextFileSentenceReader implements SentenceReader {
 	    if (!invalidPaths.isEmpty()) {
 	        throw new IOException("Some files do not exist or are unreadable: " + invalidPaths);
 	    }
-
-	    this.divider = divider;
 	}
 
 	@Override
@@ -153,5 +86,4 @@ public class TextFileSentenceReader implements SentenceReader {
 	public boolean supportsReset() {
 		return true;
 	}
-
 }
